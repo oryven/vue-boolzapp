6a0eef29f6a0392a9319f32bf7dabd01f8233a93
Orazio <orazioventrella@hotmail.it>
2021-11-15 15:55:06 +0100

    arrey di oggetti

---
+++ js/main.js
@@ -0,0 +1,130 @@
+/* esercizio boolzap
+Milestone 1 
+Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della l ista contatti: t ramite l a direttiva v-for, visualizzare
+nome e i mmagine di ogni contatto */
+
+// creiamo un'intanza per usare Vue
+
+var container = new Vue (
+
+    // richimiamo la sezione del dom
+    {
+        el: "#mainPart",
+
+        data: {
+
+            // creiamo un'array di oggetti 
+
+            contacts: [
+
+                {
+                    userName: "Michele",
+                    profileImage: "img/avatar_1.jpg",
+                    visible: true,
+                    messages: [
+
+                        {
+                            date: "10/01/2020 15:30:55",
+                            text: "Hai portato a spasso il cane?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 15:50:00",
+                            text: "Ricordati di dargli da mangiare",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 16:15:22",
+                            text: "Tutto fatto!",
+                            status: "received"
+                        }
+
+                    ],
+                },
+
+                {
+                    userName: "Fabio",
+                    profileImage: "img/avatar_2.jpg",
+                    visible: true,
+                    messages: [
+
+                        {
+                            date: "20/03/2020 16:30:55",
+                            text: "Ciao come stai?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "20/03/2020 16:30:55",
+                            text: "Bene grazie! Stasera ci vediamo?",
+                            status: "received"
+                        },
+
+                        {
+                            date: "20/03/2020 16:35:00",
+                            text: "Mi piacerebbe ma devo andare a fare la spesa",
+                            status: "sent"
+                        }
+
+                    ],
+                },
+
+                {
+                    userName: "Samuele",
+                    profileImage: "img/avatar_3.jpg",
+                    visible: true,
+                    messages: [
+
+                        {
+                            date: "28/03/2020 10:10:40",
+                            text: "La Marianna va in campagna",
+                            status: "received"
+                        },
+
+                        {
+                            date: "28/03/2020 10:20:10",
+                            text: "Sicuro di non aver sbagliato chat?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "28/03/2020 16:15:22",
+                            text: "Ah scusa!",
+                            status: "received"
+                        }
+
+                    ],
+                },
+
+                {
+                    userName: "Luisa",
+                    profileImage: "img/avatar_4.jpg",
+                    visible: true,
+                    messages: [
+
+                        {
+                            date: "10/01/2020 15:30:55",
+                            text: "Lo sai che ha aperto una nuova pizzeria?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 15:50:00",
+                            text: "Si, ma preferirei andare al cinema",
+                            status: "received"
+                        }
+
+                    ],
+                }
+
+            ]
+    
+        }
+    },
+
+)
+
